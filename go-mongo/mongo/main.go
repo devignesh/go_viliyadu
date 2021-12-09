d49058215f64283700a8d8953e518d6ea872760c
devignesh <vinesh1865@gmail.com>
2021-12-09 15:55:57 +0530

    mongo fetching and finding query

--- go-mongo/mongo/main.go
+++ go-mongo/mongo/main.go
@@ -30,31 +30,49 @@ func main() {
 	episodesCollection := quickstartDatabase.Collection("episodes")
 
 	//inserts a data into podcasts collection here bson.D is document and bson.A is array
-	podcastResult, err := podcastsCollection.InsertOne(ctx, bson.D{
-		{"title", "The Polyglot Developer Podcast"},
-		{"author", "Nic Raboy"},
-		{"tags", bson.A{"development", "programming", "coding"}},
-	})
+	// podcastResult, err := podcastsCollection.InsertOne(ctx, bson.D{
+	// 	{"title", "The Polyglot Developer Podcast"},
+	// 	{"author", "Nic Raboy"},
+	// 	{"tags", bson.A{"development", "programming", "coding"}},
+	// })
 
-	//insert a data into eposodes collection
-	episodeResult, err := episodesCollection.InsertMany(ctx, []interface{}{
-		bson.D{
-			{"podcast", podcastResult.InsertedID},
-			{"title", "GraphQL for API Development"},
-			{"description", "Learn about GraphQL from the co-creator of GraphQL, Lee Byron."},
-			{"duration", 25},
-		},
-		bson.D{
-			{"podcast", podcastResult.InsertedID},
-			{"title", "Progressive Web Application Development"},
-			{"description", "Learn about PWA development with Tara Manicsic."},
-			{"duration", 32},
-		},
-	})
+	// //insert a data into eposodes collection
+	// episodeResult, err := episodesCollection.InsertMany(ctx, []interface{}{
+	// 	bson.D{
+	// 		{"podcast", podcastResult.InsertedID},
+	// 		{"title", "GraphQL for API Development"},
+	// 		{"description", "Learn about GraphQL from the co-creator of GraphQL, Lee Byron."},
+	// 		{"duration", 25},
+	// 	},
+	// 	bson.D{
+	// 		{"podcast", podcastResult.InsertedID},
+	// 		{"title", "Progressive Web Application Development"},
+	// 		{"description", "Learn about PWA development with Tara Manicsic."},
+	// 		{"duration", 32},
+	// 	},
+	// })
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("Inserted %v documents into episode collection!\n", len(episodeResult.InsertedIDs))
+
+	//finding single
+	var podcast bson.M
+	if err = podcastsCollection.FindOne(ctx, bson.M{}).Decode(&podcast); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("podcast", podcast)
+
+	//finding all
+	cursor, err := episodesCollection.Find(ctx, bson.M{})
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Inserted %v documents into episode collection!\n", len(episodeResult.InsertedIDs))
+	var episodes []bson.M
+	if err = cursor.All(ctx, &episodes); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("episodes", episodes)
 
 	//list the number of database available in mongodb cluster
 	databases, err := client.ListDatabaseNames(ctx, bson.M{})

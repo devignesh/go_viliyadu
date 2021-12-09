4b9da4d9556dc5811ec8cb4a71d0088078f52d10
devignesh <vinesh1865@gmail.com>
2021-12-09 15:10:41 +0530

    creating db and collection and adding data into the collection

--- go-mongo/mongo/main.go
+++ go-mongo/mongo/main.go
@@ -24,6 +24,38 @@ func main() {
 	}
 	defer client.Disconnect(ctx)
 
+	//playing around create collection and db in mongo
+	quickstartDatabase := client.Database("quickstart")
+	podcastsCollection := quickstartDatabase.Collection("podcasts")
+	episodesCollection := quickstartDatabase.Collection("episodes")
+
+	//inserts a data into podcasts collection here bson.D is document and bson.A is array
+	podcastResult, err := podcastsCollection.InsertOne(ctx, bson.D{
+		{"title", "The Polyglot Developer Podcast"},
+		{"author", "Nic Raboy"},
+		{"tags", bson.A{"development", "programming", "coding"}},
+	})
+
+	//insert a data into eposodes collection
+	episodeResult, err := episodesCollection.InsertMany(ctx, []interface{}{
+		bson.D{
+			{"podcast", podcastResult.InsertedID},
+			{"title", "GraphQL for API Development"},
+			{"description", "Learn about GraphQL from the co-creator of GraphQL, Lee Byron."},
+			{"duration", 25},
+		},
+		bson.D{
+			{"podcast", podcastResult.InsertedID},
+			{"title", "Progressive Web Application Development"},
+			{"description", "Learn about PWA development with Tara Manicsic."},
+			{"duration", 32},
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Inserted %v documents into episode collection!\n", len(episodeResult.InsertedIDs))
+
 	//list the number of database available in mongodb cluster
 	databases, err := client.ListDatabaseNames(ctx, bson.M{})
 	if err != nil {

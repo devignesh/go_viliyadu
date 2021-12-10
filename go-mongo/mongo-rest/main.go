3036b72e1a9d34a9c409ff32d0d9afda54922a39
devignesh <vinesh1865@gmail.com>
2021-12-10 14:15:04 +0530

    adding create person function and remodifying the mongo conn

--- go-mongo/mongo-rest/main.go
+++ go-mongo/mongo-rest/main.go
@@ -2,18 +2,47 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func CreatePerson(w http.ResponseWriter, r *http.Request) {}
-func GetAllperson(w http.ResponseWriter, r *http.Request) {}
+var client *mongo.Client
+
+type Person struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Firstname string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	Lastname  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
+}
+
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	var person Person
+	err := json.NewDecoder(r.Body).Decode(&person)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection := client.Database("mongo-rest").Collection("person")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, err := collection.InsertOne(ctx, person)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(result)
+
+}
+func GetAllperson(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("check")
+}
 func GetPerson(w http.ResponseWriter, r *http.Request)    {}
 func UpdatePerson(w http.ResponseWriter, r *http.Request) {}
 func DeletePerson(w http.ResponseWriter, r *http.Request) {}
@@ -21,15 +50,9 @@ func DeletePerson(w http.ResponseWriter, r *http.Request) {}
 func main() {
 	fmt.Println("main function")
 
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		log.Fatal(err)
-	}
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, _ = mongo.Connect(ctx, clientOptions)
 	defer client.Disconnect(ctx)
 
 	router := mux.NewRouter()

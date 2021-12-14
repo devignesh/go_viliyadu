5b3d332f363412a852f5da48667147257edfabd7
devignesh <vinesh1865@gmail.com>
2021-12-14 11:28:46 +0530

    food controller file

---
+++ gin/api-mongo/handler/food.go
@@ -0,0 +1,61 @@
+package handler
+
+import (
+	"context"
+	"fmt"
+	"go_viliyadu/gin/api-mongo/database"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Food struct {
+	ID         primitive.ObjectID `bson:"_id"`
+	Name       *string            `json:"name" validate:"required,min=2,max=100"`
+	Price      *float64           `json:"price" validate:"required"`
+	Created_at time.Time          `json:"created_at"`
+	Updated_at time.Time          `json:"updated_at"`
+	Food_id    string             `json:"food_id"`
+}
+
+var validate = validator.New()
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func FoodCreate(c *gin.Context) {
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	var food Food
+
+	if err := c.BindJSON(&food); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	validationErr := validate.Struct(food)
+	if validationErr != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+		return
+	}
+	food.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	food.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	food.ID = primitive.NewObjectID()
+	food.Food_id = food.ID.Hex()
+
+	food.Price = food.Price
+
+	result, insertErr := foodCollection.InsertOne(ctx, food)
+	if insertErr != nil {
+		msg := fmt.Sprintf("Food item was not created")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+		return
+	}
+	defer cancel()
+
+	c.JSON(http.StatusOK, result)
+
+}

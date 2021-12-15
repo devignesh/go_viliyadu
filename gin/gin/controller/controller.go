b8607bf71504ecdb0ed5330ef19bd5a5846fe4ca
devignesh <vinesh1865@gmail.com>
2021-12-15 22:03:17 +0530

    api files

---
+++ gin/gin/controller/controller.go
@@ -0,0 +1,119 @@
+package controller
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Todo struct {
+	ID        primitive.ObjectID `json:"id" bson:"_id, omitempty"`
+	Title     string             `json:"title"`
+	Body      string             `json:"body"`
+	Completed string             `json:"completed"`
+	CreatedAt time.Time          `json:"created_at"`
+	UpdatedAt time.Time          `json:"updated_at"`
+}
+
+var collection *mongo.Collection
+
+// func Connection() *mongo.Client {
+
+// 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 	defer cancel()
+
+// 	err = client.Connect(ctx)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	// defer client.Disconnect(ctx)
+
+// 	err = client.Ping(context.Background(), readpref.Primary())
+// 	if err != nil {
+// 		log.Fatal("Couldn't connect to the database", err)
+// 	} else {
+// 		log.Println("Connected!")
+// 	}
+// 	db := client.Database("mongo_gin")
+// 	TodoCollection(db)
+// 	return client
+
+// }
+
+func TodoCollection(c *mongo.Database) {
+	collection = c.Collection("todos")
+}
+
+func CreateTodopost(c *gin.Context) {
+	var ctx, _ = context.WithTimeout(context.Background(), 100*time.Second)
+	var todo Todo
+	c.BindJSON(&todo)
+	title := todo.Title
+	body := todo.Body
+	completed := todo.Completed
+	id := primitive.NewObjectID()
+
+	newTodo := Todo{
+		ID:        id,
+		Title:     title,
+		Body:      body,
+		Completed: completed,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	_, err := collection.InsertOne(ctx, newTodo)
+
+	if err != nil {
+		log.Printf("Error while inserting new todo into db, Reason: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  http.StatusInternalServerError,
+			"message": "Something went wrong",
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"status":  http.StatusCreated,
+		"message": "Todo created Successfully",
+	})
+	return
+}
+
+func GetAllTodo(c *gin.Context) {
+	todos := []Todo{}
+	cursor, err := collection.Find(context.TODO(), bson.M{})
+
+	if err != nil {
+		log.Printf("Error while getting all todos, Reason: %v\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  http.StatusInternalServerError,
+			"message": "Something went wrong",
+		})
+		return
+	}
+
+	//iterate the document
+	for cursor.Next(context.TODO()) {
+		var todo Todo
+		cursor.Decode(&todo)
+		todos = append(todos, todo)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  http.StatusOK,
+		"message": "All Todos",
+		"data":    todos,
+	})
+	return
+}

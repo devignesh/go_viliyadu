3bd2e850d39d9ebca5b7aa2fd115d03932f1af09
devignesh <vinesh1865@gmail.com>
2021-12-15 22:02:55 +0530

    routes

---
+++ gin/gin/router/route.go
@@ -0,0 +1,12 @@
+package router
+
+import (
+	"gin/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Routes(router *gin.Engine) {
+	router.POST("/todo", controller.CreateTodopost)
+	router.GET("/todos", controller.GetAllTodo)
+}

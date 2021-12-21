fa1c0a3d5c277c965aae9e31009d3386af7e9316
devignesh <vinesh1865@gmail.com>
2021-12-21 21:46:26 +0530

    routes for delete function

--- gin/gin/router/route.go
+++ gin/gin/router/route.go
@@ -10,6 +10,7 @@ func Routes(router *gin.Engine) {
 	router.POST("/todo", controller.CreateTodopost)
 	router.GET("/todos", controller.GetAllTodo)
 	router.GET("/todo/:id", controller.GetTodo)
-	router.GET("/todo/:id", controller.UpdateTodo)
+	router.PUT("/todo/:id", controller.UpdateTodo)
+	router.DELETE("/todo/:id", controller.DeleteTodo)
 
 }

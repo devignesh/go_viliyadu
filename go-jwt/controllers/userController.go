616329fbda486f31db9950564a7b91686cc0946d
devignesh <vinesh1865@gmail.com>
2021-12-31 00:33:00 +0530

    adding signup and login functionality

--- go-jwt/controllers/userController.go
+++ go-jwt/controllers/userController.go
@@ -49,41 +49,12 @@ func Signup(c *gin.Context) {
 	if count > 0 {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "this email or phone number already exists"})
 	}
-}
-
-func Login()
-
-func GetUsers()
-
-func GetUser(c *gin.Context) {
-
-	userId := c.Param("user_id")
-
-	if err := helpers.MatchUserTypeToUid(c, userId); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-
-	var user models.User
-
-	err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
-	defer cancel()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"Message": "requst accepted",
-		"data":    user,
-	})
 
 	user.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 	user.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 	user.ID = primitive.NewObjectID()
 	user.User_id = user.ID.Hex()
-	token, refreshToken, _ := helper.GenerateAllTokens(*user.Email, *user.First_name, *user.Last_name, *user.User_type, *&user.User_id)
+	token, refreshToken, _ := helpers.GenerateAllTokens(*user.Email, *user.First_name, *user.Last_name, *user.User_type, *&user.User_id)
 	user.Token = &token
 	user.Refresh_token = &refreshToken
 
@@ -95,4 +66,59 @@ func GetUser(c *gin.Context) {
 	}
 	defer cancel()
 	c.JSON(http.StatusOK, resultInsertionNumber)
+}
+
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		var foundUser models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		err := userCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "email or password is incorrect"})
+			return
+		}
+
+		passwordIsValid, msg := VerifyPassword(*user.Password, *foundUser.Password)
+		defer cancel()
+
+	}
+}
+
+func GetUsers()
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		userId := c.Param("user_id")
+
+		if err := helpers.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User
+
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"Message": "requst accepted",
+			"data":    user,
+		})
+
+	}
 }

289d112aa878bd4c891d7c4647b3a5719a096e27
devignesh <vinesh1865@gmail.com>
2021-12-30 23:46:19 +0530

    getuser api functionality

--- go-jwt/controllers/userController.go
+++ go-jwt/controllers/userController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"context"
 	"go_viliyadu/go-jwt/database"
+	"go_viliyadu/go-jwt/helpers"
 	"go_viliyadu/go-jwt/models"
 	"log"
 	"net/http"
@@ -53,5 +54,27 @@ func Login()
 func GetUsers()
 
 func GetUser(c *gin.Context) {
+
+	userId := c.Param("user_id")
+
+	if err := helpers.MatchUserTypeToUid(c, userId); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	var user models.User
+
+	err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+	defer cancel()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"Message": "requst accepted",
+		"data":    user,
+	})
 
 }

57b5bdb65c928ca64d738d7529e6ea2bebb5a1ac
devignesh <vinesh1865@gmail.com>
2021-12-30 23:07:08 +0530

    adding routes to the application

--- go-jwt/routes/userrouter.go
+++ go-jwt/routes/userrouter.go
@@ -1,1 +1,12 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-chi/chi/middleware"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controllers.GetUsers())
+	incomingRoutes.GET("/users/:user_id", controllers.GetUser())
+}

4833871723a64b05e9edee1c1b1f4ab76a4f4953
devignesh <vinesh1865@gmail.com>
2022-01-01 23:54:22 +0530

    routes

--- go-jwt/routes/userrouter.go
+++ go-jwt/routes/userrouter.go
@@ -4,11 +4,10 @@ import (
 	"go_viliyadu/go-jwt/controllers"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-chi/chi/middleware"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.Use(middleware.Authenticate())
+	// incomingRoutes.Use(middleware.Authenticate())
 	incomingRoutes.GET("/users", controllers.GetUsers())
 	incomingRoutes.GET("/users/:user_id", controllers.GetUser())
 }

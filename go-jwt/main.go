417454d2ac31bc074beba70b1abbdd01ea0064f9
devignesh <vinesh1865@gmail.com>
2022-01-02 00:24:17 +0530

    remove the error code

--- go-jwt/main.go
+++ go-jwt/main.go
@@ -23,21 +23,22 @@ func main() {
 
 	if port == "" {
 		port = "8000"
-
-		router := gin.New()
-		router.Use(gin.Logger())
-
-		routes.AuthRoutes(router)
-		routes.UserRoutes(router)
-
-		router.GET("/api-1", func(c *gin.Context) {
-			c.JSON(200, gin.H{"success": "Access granted for api-1"})
-		})
-
-		router.GET("/api-2", func(c *gin.Context) {
-			c.JSON(200, gin.H{"success": "Access granted for api-2"})
-		})
-
-		router.Run(":" + port)
 	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-1"})
+	})
+
+	router.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+
+	router.Run(":" + port)
+
 }

7eb758128a17f4f2d1b566cc89b97b1d7dd15684
devignesh <vinesh1865@gmail.com>
2022-01-04 21:16:43 +0530

    changing routes

--- go-jwt/routes/userrouter.go
+++ go-jwt/routes/userrouter.go
@@ -8,7 +8,7 @@ import (
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.Use(middleware.Authenticate())
-	incomingRoutes.GET("/users", controllers.GetUsers())
-	incomingRoutes.GET("/users/:user_id", controllers.GetUser())
+	incomingRoutes.Use(middleware.Authenticate)
+	incomingRoutes.GET("/users", controllers.GetUsers)
+	incomingRoutes.GET("/users/:user_id", controllers.GetUser)
 }

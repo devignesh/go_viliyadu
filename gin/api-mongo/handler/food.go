9cab182261e68e1b4705c52ffa35dc58ffc74f94
Vignesh kumar Dharmalingam <vinesh1865@gmail.com>
2022-02-21 22:42:04 +0530

    Update food.go

--- gin/api-mongo/handler/food.go
+++ gin/api-mongo/handler/food.go
@@ -13,6 +13,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+//food struct creation
 type Food struct {
 	ID         primitive.ObjectID `bson:"_id"`
 	Name       *string            `json:"name" validate:"required,min=2,max=100"`

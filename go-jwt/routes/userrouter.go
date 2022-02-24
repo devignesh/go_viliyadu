6e74a2c45a60e9560bb92c232a4b91a21a0f8183
Vignesh kumar Dharmalingam <vinesh1865@gmail.com>
2022-02-24 22:01:34 +0530

    Update userrouter.go

--- go-jwt/routes/userrouter.go
+++ go-jwt/routes/userrouter.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+//user routes func
 func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.Use(middleware.Authenticate)
 	incomingRoutes.GET("/users", controllers.GetUsers)

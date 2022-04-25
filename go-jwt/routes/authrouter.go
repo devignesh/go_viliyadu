09ef0f3842f0436c7f14130f4f26823a29e80cf0
Vignesh kumar Dharmalingam <vinesh1865@gmail.com>
2022-04-25 22:24:41 +0530

    Update authrouter.go

--- go-jwt/routes/authrouter.go
+++ go-jwt/routes/authrouter.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+//auth routes
 func AuthRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("users/signup", controllers.Signup)
 	incomingRoutes.POST("users/login", controllers.Login)

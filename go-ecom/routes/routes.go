7f6c834aa202768a2304647e8147b60fed553354
devignesh <vinesh1865@gmail.com>
2022-11-01 21:19:10 +0530

    added the intial routes

--- go-ecom/routes/routes.go
+++ go-ecom/routes/routes.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	"go-hotel/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(routes *gin.Engine) {
+
+	routes.POST("/users/signup", controllers.SignUp())
+	routes.POST("/users/login", controllers.Login())
+	routes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	routes.POST("/users/productview", controllers.SearchProduct())
+	routes.POST("/users/search", controllers.SearchProductByQuery())
+
+}

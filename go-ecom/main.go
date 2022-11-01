21a4af446d48c3dbeb5dc415bb6e45ecbcefb600
devignesh <vinesh1865@gmail.com>
2022-11-01 21:31:10 +0530

    intial main filr

--- go-ecom/main.go
+++ go-ecom/main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"go_viliyadu/go-ecom/middleware"
+	"go_viliyadu/go-ecom/routes"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("main func")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UserRouters(router)
+	router.Use(middleware.Authentications())
+
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
 }

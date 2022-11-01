287ab2c3c3a662cedecb6d8d4050323abb949097
devignesh <vinesh1865@gmail.com>
2022-11-01 22:18:57 +0530

    added the controller skelton func

--- go-ecom/controllers/controllers.go
+++ go-ecom/controllers/controllers.go
@@ -1,1 +1,15 @@
 package controllers
+
+func HashPassword()
+
+func VerifyPassword()
+
+func SignUpCon()
+
+func LoginCon()
+
+func ProductViewerAdmin()
+
+func SearchProduct()
+
+func SearchProductByQuery()

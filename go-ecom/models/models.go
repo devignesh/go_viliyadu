09bcf95f358fab55cb118a5aad44a2149209fe80
devignesh <vinesh1865@gmail.com>
2022-11-01 21:59:32 +0530

    added the supported models

--- go-ecom/models/models.go
+++ go-ecom/models/models.go
@@ -32,26 +32,26 @@ type Product struct {
 }
 
 type ProductUser struct {
-	ProductID
-	ProductName
-	Price
-	Ratings
-	Image
+	ProductID   primitive.ObjectID `bson:"_id"`
+	ProductName *string            `json:"product_name" bson:"product_name"`
+	Price       *uint64            `json:"price"`
+	Ratings     *uint8             `json:"ratings"`
+	Image       *string            `json:"image"`
 }
 type Address struct {
-	AddressID
-	House
-	Street
-	City
-	Pincode
+	AddressID primitive.ObjectID `bson:"_id"`
+	House     *string            `json:"house_name" bson:"house_name"`
+	Street    *string            `json:"street_name" bson:"street_name"`
+	City      *string            `json:"city_name" bson:"city_name"`
+	Pincode   *string            `json:"pin_code" bson:"pin_code"`
 }
 type Order struct {
-	OrderID
-	OrderCart
-	OrderedAt
-	Price
-	Discount
-	PaymentMethod
+	OrderID       primitive.ObjectID `bson:"_id"`
+	OrderCart     []ProductUser      `json:"order_list"  bson:"order_list"`
+	OrderedAt     time.Time          `json:"ordered_on"  bson:"ordered_on"`
+	Price         int                `json:"total_price" bson:"total_price"`
+	Discount      *int               `json:"discount"    bson:"discount"`
+	PaymentMethod Payment            `json:"payment_method" bson:"payment_method"`
 }
 
 type Payment struct {

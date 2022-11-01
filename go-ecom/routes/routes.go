b6518431b540a41f87a936ffdcc041ceb5a4c7b4
devignesh <vinesh1865@gmail.com>
2022-11-01 21:20:49 +0530

    added the intial routes

--- go-ecom/routes/routes.go
+++ go-ecom/routes/routes.go
@@ -8,8 +8,8 @@ import (
 
 func UserRoutes(routes *gin.Engine) {
 
-	routes.POST("/users/signup", controllers.SignUp())
-	routes.POST("/users/login", controllers.Login())
+	routes.POST("/users/signup", controllers.SignUpCon())
+	routes.POST("/users/login", controllers.LoginCon())
 	routes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
 	routes.POST("/users/productview", controllers.SearchProduct())
 	routes.POST("/users/search", controllers.SearchProductByQuery())

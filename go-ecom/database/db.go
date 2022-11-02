cc9886e899ee0a59b8e2f978cf68bbb13bc69663
devignesh <vinesh1865@gmail.com>
2022-11-02 21:26:07 +0530

    setup the db conn

--- go-ecom/database/db.go
+++ go-ecom/database/db.go
@@ -1,8 +1,38 @@
 package database
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func DBSet() *mongo.Client {
+	Mongodb := "mongodb://localhost:27017"
+	client, err := mongo.NewClient(options.Client().ApplyURI(Mongodb))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Println("failed to connect to mongodb")
+		return nil
+	}
+
+	fmt.Println("Successfully Connected to the mongodb")
+	return client
 
 }
 

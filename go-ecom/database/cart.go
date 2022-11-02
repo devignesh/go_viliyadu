16ab8bdb560cbfae3213a8c15070725811c3ed4a
devignesh <vinesh1865@gmail.com>
2022-11-02 21:16:47 +0530

    added the initial custom error type

--- go-ecom/database/cart.go
+++ go-ecom/database/cart.go
@@ -1,5 +1,17 @@
 package database
 
+import "errors"
+
+var (
+	ErrCantFindProduct    = errors.New("can't find product")
+	ErrCantDecodeProducts = errors.New("can't find product")
+	ErrUserIdIsNotValid   = errors.New("user is not valid")
+	ErrCantUpdateUser     = errors.New("can't add product to cart")
+	ErrCantRemoveItem     = errors.New("can't remove item from cart")
+	ErrCantGetItem        = errors.New("can't get item from cart")
+	ErrCantBuyCartItem    = errors.New("can't update the purchase")
+)
+
 func AddProductToCart() {}
 
 func RemoveCartItem() {}

0fedbe0600eaf4af3e732b3c4c9d6dd5e05da20f
devignesh <vinesh1865@gmail.com>
2022-11-02 21:19:48 +0530

    intial db func intialization

--- go-ecom/database/db.go
+++ go-ecom/database/db.go
@@ -1,1 +1,11 @@
 package database
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+func DBSet() *mongo.Client {
+
+}
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {}

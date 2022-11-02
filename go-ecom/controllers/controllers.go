a4148a7fb028c687a64c12f1c0d0fd32030cfcea
devignesh <vinesh1865@gmail.com>
2022-11-02 21:58:26 +0530

    added the signup func

--- go-ecom/controllers/controllers.go
+++ go-ecom/controllers/controllers.go
@@ -1,12 +1,91 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"go_viliyadu/go-ecom/database"
+	dto "go_viliyadu/go-ecom/models"
+	"go_viliyadu/go-ecom/token"
+
+	"github.com/gin-gonic/gin"
+	validate "github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var UserCollection *mongo.Collection = database.UserData(database.ClientMongo, "Users")
 
 func HashPassword(password string) string {}
 
 func VerifyPassword(userPassword, givenPassword string) (bool, string) {}
 
 func SignUpCon() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user dto.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+			return
+		}
+
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "User already exists"})
+		}
+
+		count, err = UserCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Phone is already in use"})
+			return
+		}
+
+		password := HashPassword(*user.Password)
+		user.Password = &password
+
+		user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.UserID = user.ID.Hex()
+		token, refreshtoken, _ := token.TokenGenerator(*user.Email, *user.FistName, *user.LastName, user.UserID)
+		user.Token = &token
+		user.RefreshToken = &refreshtoken
+		user.UserCart = make([]dto.ProductUser, 0)
+		user.AddressDetails = make([]dto.Address, 0)
+		user.OrderStatus = make([]dto.Order, 0)
+
+		_, inserterr := UserCollection.InsertOne(ctx, user)
+		if inserterr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "not created"})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusCreated, "Successfully Signed Up!!")
+
+	}
 
 }
 
